91be728daeaa4cf01941d5b1fcdb7fe294d959d1
SIDDHUGITTY <142041319+SIDDHUGITTY@users.noreply.github.com>
2024-02-09 00:06:28 +0530

    Update urls.py

--- project/mysite/mysite/urls.py
+++ project/mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', include('hello_world.urls')),
+    path('', include('hello_world.urls')),
    
 
 ]
