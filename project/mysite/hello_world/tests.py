1d84e2a0eed33475fb1387124f9ec4f85bd1dc9a
SIDDHUGITTY <142041319+SIDDHUGITTY@users.noreply.github.com>
2024-02-24 17:44:37 +0530

    Update tests.py

--- project/mysite/hello_world/tests.py
+++ project/mysite/hello_world/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HelloWorldTestCase(TestCase):
+
+    def test_hello_world(self):
+
+        response = self.client.get('Hello World!')
